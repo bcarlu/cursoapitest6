12ab9e63786f32fd27757026382ede05ea5e9109
Brian <brian@brianpc>
2023-04-24 10:51:41 -0500

    url shortener added

--- index.js
+++ index.js
@@ -10,13 +10,31 @@ app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+let dataShortener = []
+
+app.post('/api/shorturl', function(req, res) {
+  try{
+    let url = new URL(req.body.url)
+    let obj = { original_url : url.href, short_url : Math.floor(Math.random() * 10000)}
+    dataShortener.push(obj)
+    res.json(obj);
+  } catch {
+    res.json({ error: 'invalid url'})
+  }
+  /** ## to do: Validate if url was already shortened */ 
+});
+
+app.get('/api/shorturl/:id', function(req, res) {
+  let id = req.params.id
+  let reqUrl = dataShortener.filter((value) => { return value.short_url == id})
+  res.redirect(reqUrl[0].original_url);
 });
 
 app.listen(port, function() {
