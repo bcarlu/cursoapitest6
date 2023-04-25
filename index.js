b714321c50fdcbac7824e46d08a801f4b0b2133b
Brian <brian@brianpc>
2023-04-24 22:46:26 -0500

    fix 2 url shortener added

--- index.js
+++ index.js
@@ -20,20 +20,34 @@ app.get('/', function(req, res) {
 let dataShortener = []
 
 app.post('/api/shorturl', function(req, res) {
-  if (req.body.url == "") {
-    res.json({ error: 'invalido'})
+  if(req.body.url == ""){
+    res.json({ error: 'invalid url'})
   }
 
-  try{
-    let url = new URL(req.body.url)
-    let obj = { original_url : url.href, short_url : Math.floor(Math.random() * 10000)}
+  // let pattern = /^(http:\/\/|https:\/\/)[a-z0-9]+.[a-z]+.*[a-z0-9]*/
+  let pattern = /^https?:\/\/[\w\-]+(\.[\w\-]+)+[/#?]?.*$/
+ 
+  if(req.body.url.match(pattern)){
+    let obj = { original_url : req.body.url, short_url : Math.floor(Math.random() * 10000)}
     dataShortener.push(obj)
     res.json(obj);
-  } catch {
+  } else {
     res.json({ error: 'invalid url'})
   }
   /** ## to do: Validate if url was already shortened */ 
 });
+
+// app.post('/api/shorturl', function(req, res) {
+//   try{
+//     let url = new URL(req.body.url)
+//     let obj = { original_url : url.href, short_url : Math.floor(Math.random() * 10000)}
+//     dataShortener.push(obj)
+//     res.json(obj);
+//   } catch {
+//     res.json({ error: 'invalid url'})
+//   }
+//   /** ## to do: Validate if url was already shortened */ 
+// });
 
 app.get('/api/shorturl/:id', function(req, res) {
   let id = req.params.id
